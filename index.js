191f3d19694052c41883cf0d1a4368be1851a031
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-11 17:41:24 +0400

    animation on input and text boxes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const commentButton = document.querySelector(".comment-button");
 const nihat = document.querySelector(".div-nihat");
 const input = document.querySelector(".input");
+const resetButton = document.querySelector(".reset-button");
 
 commentButton.addEventListener('click', e => {
-  let myImage = document.createElement("img");
-  myImage.src = "file:////Users/Mammadov/Desktop/ADA_Nihat/messenger/photoes/avatar4 copy.svg";
+  // let myImage = document.createElement("img");
+  // myImage.src = "file:////Users/Mammadov/Desktop/ADA_Nihat/messenger/photoes/avatar4 copy.svg";
   const myComment = document.createElement("div");
 
-  myImage.classList.add("myImage");
+  // myImage.classList.add("myImage");
   myComment.classList.add("myComment");
   myComment.innerHTML = input.value;
 
@@ -15,10 +16,17 @@ commentButton.addEventListener('click', e => {
 
   console.log(contanier);
   contanier.appendChild(myComment);
-  myComment.appendChild(myImage);
+  // myComment.appendChild(myImage);
 
   input.value = "";
-
+  contanier.style.height = '600px'
+  contanier.style.height += '200px'
 })
 
 // when you submit without a comment it still submit
+
+
+
+resetButton.addEventListener('click', e=> [
+  input.value = ""
+]);
