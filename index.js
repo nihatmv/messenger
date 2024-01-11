33128037de53f20f59ef60daa4052ace0f090fe1
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-11 17:50:31 +0400

    blocking the empty messages

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const input = document.querySelector(".input");
 const resetButton = document.querySelector(".reset-button");
 
 commentButton.addEventListener('click', e => {
-  // let myImage = document.createElement("img");
-  // myImage.src = "file:////Users/Mammadov/Desktop/ADA_Nihat/messenger/photoes/avatar4 copy.svg";
+  if (input.value === "") {
+    alert("You cannot send an emtpy message")
+    return
+  }
   const myComment = document.createElement("div");
 
-  // myImage.classList.add("myImage");
   myComment.classList.add("myComment");
   myComment.innerHTML = input.value;
 
   const contanier = document.querySelector(".contanier");
 
-  console.log(contanier);
   contanier.appendChild(myComment);
-  // myComment.appendChild(myImage);
+
+  
 
   input.value = "";
-  contanier.style.height = '600px'
-  contanier.style.height += '200px'
 })
+
+
 
 // when you submit without a comment it still submit
 
