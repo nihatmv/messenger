649073a706008a69ad630e9a0a1acde702b6d336
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-12 19:42:37 +0400

    pressing enter on keyboard

--- index.js
+++ index.js
@@ -2,37 +2,39 @@ const commentButton = document.querySelector(".comment-button");
 const nihat = document.querySelector(".div-nihat");
 const input = document.querySelector(".input");
 const resetButton = document.querySelector(".reset-button");
+const contanier = document.querySelector(".contanier");
 
 let inputs = [];
 let outputs = [];
 
 
 let paddingValue = 20;
+let myComment = document.createElement("div");
+
+
+
 commentButton.addEventListener('click', e => {
-
-
+  
   if (input.value === "") {
     alert("You cannot send an emtpy message")
     return
   }
 
-  const contanier = document.querySelector(".contanier");
-  const deleteButton = document.createElement("button");
-  const myComment = document.createElement("div");
-  const comments = document.createElement("div");
-
-  deleteButton.classList.add("deleteButton");
+  let myComment = document.createElement("div");
+  let deleteButton = document.createElement("button");
+  let comments = document.createElement("div");
   myComment.classList.add("myComment");
+  deleteButton.classList.add("deleteButton");
   comments.classList.add("comments");
+
+  
+
   myComment.innerHTML = input.value;
   deleteButton.innerHTML = 'Delete'
 
   inputs.push(input.value);
-
   outputs.push(myComment.innerHTML);
 
-  // console.log(inputs);
-  // console.log(outputs);
 
   contanier.appendChild(comments);
   comments.appendChild(myComment);
@@ -41,21 +43,64 @@ commentButton.addEventListener('click', e => {
   input.value = "";
 
   paddingValue += 25
-  // window.resizeTo(window.innerWidth, window.innerHeight + 30);
   document.body.style.paddingTop = paddingValue + "px";
   contanier.style.height = (contanier.clientHeight + 50) + "px";
 
 
   deleteButton.addEventListener('click', e => {
-    // console.log(myComment.outerText);
-
     if (inputs[0] === outputs[0]) {
       comments.remove();
     }
+  });
 
-});
+})
 
 
+
+input.addEventListener('keypress', e => {
+  if (e.key === "Enter") {
+
+    if (input.value === "") {
+      alert("You cannot send an emtpy message")
+      return
+    }
+  
+    let myComment = document.createElement("div");
+    let deleteButton = document.createElement("button");
+    let comments = document.createElement("div");
+    myComment.classList.add("myComment");
+    deleteButton.classList.add("deleteButton");
+    comments.classList.add("comments");
+  
+    
+  
+    myComment.innerHTML = input.value;
+    deleteButton.innerHTML = 'Delete'
+  
+    inputs.push(input.value);
+    outputs.push(myComment.innerHTML);
+  
+  
+    contanier.appendChild(comments);
+    comments.appendChild(myComment);
+    comments.appendChild(deleteButton);
+  
+    input.value = "";
+  
+    paddingValue += 25
+    document.body.style.paddingTop = paddingValue + "px";
+    contanier.style.height = (contanier.clientHeight + 50) + "px";
+  
+  
+    deleteButton.addEventListener('click', e => {
+      if (inputs[0] === outputs[0]) {
+        comments.remove();
+      }
+    });
+
+
+
+  }
 })
 
 
