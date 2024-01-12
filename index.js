e91a204650c8f0f42594d6f519578cf7a1ccd29d
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-12 15:01:57 +0400

    active delete button

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const nihat = document.querySelector(".div-nihat");
 const input = document.querySelector(".input");
 const resetButton = document.querySelector(".reset-button");
 
+let inputs = [];
+let outputs = [];
 
 
 let paddingValue = 20;
 commentButton.addEventListener('click', e => {
+
+
   if (input.value === "") {
     alert("You cannot send an emtpy message")
     return
@@ -23,6 +27,12 @@ commentButton.addEventListener('click', e => {
   myComment.innerHTML = input.value;
   deleteButton.innerHTML = 'Delete'
 
+  inputs.push(input.value);
+
+  outputs.push(myComment.innerHTML);
+
+  // console.log(inputs);
+  // console.log(outputs);
 
   contanier.appendChild(comments);
   comments.appendChild(myComment);
@@ -30,16 +40,19 @@ commentButton.addEventListener('click', e => {
 
   input.value = "";
 
-  paddingValue += 10
+  paddingValue += 25
   // window.resizeTo(window.innerWidth, window.innerHeight + 30);
   document.body.style.paddingTop = paddingValue + "px";
-  contanier.style.height = (contanier.clientHeight + 30) + "px";
-  console.log(deleteButton);
+  contanier.style.height = (contanier.clientHeight + 50) + "px";
 
 
   deleteButton.addEventListener('click', e => {
-    
-    console.log(myComment.outerText);
+    // console.log(myComment.outerText);
+
+    if (inputs[0] === outputs[0]) {
+      comments.remove();
+    }
+
 });
 
 
