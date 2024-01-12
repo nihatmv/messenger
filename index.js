fca2ec4ecbdecd3010197f6a29331dbd2697a23f
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-12 14:34:53 +0400

    delete button

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const nihat = document.querySelector(".div-nihat");
 const input = document.querySelector(".input");
 const resetButton = document.querySelector(".reset-button");
 
+
+
 let paddingValue = 20;
 commentButton.addEventListener('click', e => {
   if (input.value === "") {
@@ -10,25 +12,42 @@ commentButton.addEventListener('click', e => {
     return
   }
 
-  const myComment = document.createElement("div");
-
-  myComment.classList.add("myComment");
-  myComment.innerHTML = input.value;
-
   const contanier = document.querySelector(".contanier");
+  const deleteButton = document.createElement("button");
+  const myComment = document.createElement("div");
+  const comments = document.createElement("div");
 
-  contanier.appendChild(myComment);
+  deleteButton.classList.add("deleteButton");
+  myComment.classList.add("myComment");
+  comments.classList.add("comments");
+  myComment.innerHTML = input.value;
+  deleteButton.innerHTML = 'Delete'
+
+
+  contanier.appendChild(comments);
+  comments.appendChild(myComment);
+  comments.appendChild(deleteButton);
+
+  input.value = "";
 
   paddingValue += 10
-  input.value = "";
-  window.resizeTo(window.innerWidth, window.innerHeight + 30);
+  // window.resizeTo(window.innerWidth, window.innerHeight + 30);
   document.body.style.paddingTop = paddingValue + "px";
   contanier.style.height = (contanier.clientHeight + 30) + "px";
+  console.log(deleteButton);
+
+
+  deleteButton.addEventListener('click', e => {
+    
+    console.log(myComment.outerText);
+});
 
 
 })
 
 
+
 resetButton.addEventListener('click', e=> [
   input.value = ""
 ]);
+
