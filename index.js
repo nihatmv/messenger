5e9ac971c24dc97680396eb3728e8ad330f33581
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-12 11:39:56 +0400

    adding margin each time

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const nihat = document.querySelector(".div-nihat");
 const input = document.querySelector(".input");
 const resetButton = document.querySelector(".reset-button");
 
+let paddingValue = 20;
 commentButton.addEventListener('click', e => {
   if (input.value === "") {
     alert("You cannot send an emtpy message")
     return
   }
+
   const myComment = document.createElement("div");
 
   myComment.classList.add("myComment");
@@ -17,8 +19,13 @@ commentButton.addEventListener('click', e => {
 
   contanier.appendChild(myComment);
 
-
+  paddingValue += 10
   input.value = "";
+  window.resizeTo(window.innerWidth, window.innerHeight + 30);
+  document.body.style.paddingTop = paddingValue + "px";
+  contanier.style.height = (contanier.clientHeight + 30) + "px";
+
+
 })
 
 
