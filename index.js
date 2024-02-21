e8bfd84edd0a1a6b9a97005270165369774dfa31
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-02-21 23:19:24 +0400

    change

--- index.js
+++ index.js
@@ -8,18 +8,15 @@ let inputs = [];
 let outputs = [];
 let noCharactersRegex = /^\s*$/;
 
-
 let paddingValue = 20;
 let myComment = document.createElement("div");
 
-
-commentButton.addEventListener('click', e => {
-  
+commentButton.addEventListener("click", (e) => {
   if (noCharactersRegex.test(input.value)) {
-    alert("You cannot send an empty message")
-    return
+    alert("You cannot send an empty message");
+    return;
   } else {
-    console.log('hello');
+    console.log("hello");
 
     let myComment = document.createElement("div");
     let deleteButton = document.createElement("button");
@@ -27,91 +24,71 @@ commentButton.addEventListener('click', e => {
     myComment.classList.add("myComment");
     deleteButton.classList.add("deleteButton");
     comments.classList.add("comments");
-  
-  
+
     myComment.innerHTML = input.value;
-    deleteButton.innerHTML = 'Delete'
-  
+    deleteButton.innerHTML = "Delete";
+
     inputs.push(input.value);
     outputs.push(myComment.innerHTML);
-  
-  
+
     contanier.appendChild(comments);
     comments.appendChild(myComment);
     comments.appendChild(deleteButton);
-  
-    input.value = "";
-  
-    paddingValue += 25
-    document.body.style.paddingTop = paddingValue + "px";
-    contanier.style.height = (contanier.clientHeight + 50) + "px";
 
-  
-  
-    deleteButton.addEventListener('click', e => {
+    input.value = "";
+
+    paddingValue += 25;
+    document.body.style.paddingTop = paddingValue + "px";
+    contanier.style.height = contanier.clientHeight + 50 + "px";
+
+    deleteButton.addEventListener("click", (e) => {
       if (inputs[0] === outputs[0]) {
         comments.remove();
       }
-    
-  });
-      
+    });
   }
+});
 
-})
-
-
-input.addEventListener('keypress', e => {
-  
+input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-
-
-
     if (noCharactersRegex.test(input.value)) {
-      alert("You cannot send an empty message")
-      e.preventDefault()
+      alert("You cannot send an empty message");
+      e.preventDefault();
 
-      input.value = "";    
-      return
+      input.value = "";
+      return;
     } else {
-
       let myComment = document.createElement("div");
       let deleteButton = document.createElement("button");
       let comments = document.createElement("div");
       myComment.classList.add("myComment");
       deleteButton.classList.add("deleteButton");
       comments.classList.add("comments");
-    
+
       myComment.innerHTML = input.value;
-      deleteButton.innerHTML = 'Delete'
-    
+      deleteButton.innerHTML = "Delete";
+
       inputs.push(input.value);
       outputs.push(myComment.innerHTML);
-    
-    
+
       contanier.appendChild(comments);
       comments.appendChild(myComment);
       comments.appendChild(deleteButton);
-      
-      e.preventDefault()
-      input.value = "";
-    
-      paddingValue += 25
-      document.body.style.paddingTop = paddingValue + "px";
-      contanier.style.height = (contanier.clientHeight + 50) + "px";
 
-    
-    
-      deleteButton.addEventListener('click', e => {
+      e.preventDefault();
+      input.value = "";
+
+      paddingValue += 25;
+      document.body.style.paddingTop = paddingValue + "px";
+      contanier.style.height = contanier.clientHeight + 50 + "px";
+
+      deleteButton.addEventListener("click", (e) => {
         if (inputs[0] === outputs[0]) {
           comments.remove();
         }
-      
-        });
+      });
     }
   }
-})
+});
 
-resetButton.addEventListener('click', e=> [
-  input.value = ""
-]);
-
+resetButton.addEventListener("click", (e) => [(input.value = "")]);
